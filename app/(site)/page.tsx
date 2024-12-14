9309a74ff6de34045a30a2cfc1255c44dd491515
tony9020 <tonychristopherofficial@gmail.com>
2024-12-14 11:51:54 +0530

    testing
    Signed-off-by: tony9020 <tonychristopherofficial@gmail.com>

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,8 +14,8 @@ import Blog from "@/components/Blog";
 import Testimonial from "@/components/Testimonial";
 
 export const metadata: Metadata = {
-  title: "Next.js Starter Template for SaaS Startups - Solid SaaS Boilerplate",
-  description: "This is Home for Solid Pro",
+  title: "Kuwait International Treatment Abroad (KITA) Conference & Inhealth Exhibition ",
+  description: "Conference & Inhealth Exhibition",
   // other metadata
 };
 
